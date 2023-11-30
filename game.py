b40f9190d09f830d1ca32fffa450616fbbc05291
nihaltp <nihaltpnki@gmail.com>
2023-11-30 18:03:51 +0530

    Added highlight
    now when we click on a square on cheessboard it is highlighted using a new function, when we touch outside the chessboard it will disappear

--- game.py
+++ game.py
@@ -1,14 +1,25 @@
 import sys
+import chess
 import pygame
 
 # Initialize Pygame
 pygame.init()
 
 # Constants
-width, height = 600, 500
+width = 700
+height = 500
 square_width = 400
-rows, columns = 8, 8
+rows = 8
+columns = 8
 square_size = square_width // columns
+
+# Where to start the chess board
+chess_x = 150
+chess_y = 50
+
+# Variables
+selected_piece = None
+highlight = False
 
 #import images
 pawn_w = pygame.image.load("pieces/pawn_w.png")
@@ -24,11 +35,23 @@ queen_b = pygame.image.load("pieces/queen_b.png")
 king_w = pygame.image.load("pieces/king_w.png")
 king_b = pygame.image.load("pieces/king_b.png")
 
-# Introduce a variable to track the selected piece
-selected_piece = None
+piece_image = {
+    "pawn_w": pawn_w,
+    "rook_w": rook_w,
+    "knight_w": knight_w,
+    "bishop_w": bishop_w,
+    "queen_w": queen_w,
+    "king_w": king_w,
+    "pawn_b": pawn_b,
+    "rook_b": rook_b,
+    "knight_b": knight_b,
+    "bishop_b": bishop_b,
+    "queen_b": queen_b,
+    "king_b": king_b,
+}
 
 # Complete Chessboard with Pieces
-chessboard_dict = {
+chessboard = {
     'a1': 'rook_w', 'b1': 'knight_w', 'c1': 'bishop_w', 'd1': 'queen_w', 'e1': 'king_w', 'f1': 'bishop_w', 'g1': 'knight_w', 'h1': 'rook_w',
     'a2': 'pawn_w', 'b2': 'pawn_w', 'c2': 'pawn_w', 'd2': 'pawn_w', 'e2': 'pawn_w', 'f2': 'pawn_w', 'g2': 'pawn_w', 'h2': 'pawn_w',
     'a3': 'NA', 'b3': 'NA', 'c3': 'NA', 'd3': 'NA', 'e3': 'NA', 'f3': 'NA', 'g3': 'NA', 'h3': 'NA',
@@ -39,29 +62,33 @@ chessboard_dict = {
     'a8': 'rook_b', 'b8': 'knight_b', 'c8': 'bishop_b', 'd8': 'queen_b', 'e8': 'king_b', 'f8': 'bishop_b', 'g8': 'knight_b', 'h8': 'rook_b',
 }
 
+
+FILE_NAMES = ["a", "b", "c", "d", "e", "f", "g", "h"]
+RANK_NAMES = ["1", "2", "3", "4", "5", "6", "7", "8"]
+
+# Draw the chessboard# Create a 2D list to represent the chessboard with values
+squares = [
+    "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
+    "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+    "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+    "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+    "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+    "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+    "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+    "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+]
+
 # Colors
 white = (255, 255, 255)
 black = (192, 192, 192)
+gold = (255, 215, 0)
 text_color = white
+background = (135, 206, 250)
+board_border = (0, 0, 0)
 
 # Create the Pygame window
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("AI Chess")
-
-# Where to start the chess board
-chess_x = 100
-chess_y = 50
-
-chessboard_rows = [8,7,6,5,4,3,2,1]
-chessboard_columns = ["a","b","c","d","e","f","g","h"]
-
-# Draw the chessboard# Create a 2D list to represent the chessboard with values
-chessboard = [[0] * columns for _ in range(rows)]
-
-# Assign unique values to each square
-for row in range(rows):
-    for column in range(columns):
-        chessboard[row][column] = chessboard_columns[column], chessboard_rows[row]
 
 # Pygame font setup
 pygame.font.init()
@@ -69,6 +96,12 @@ font = pygame.font.SysFont(None, 30)
 
 # Draw chessboard
 def draw_chessboard(rows, columns):
+    global highlight, mouse_x, mouse_y
+
+    screen.fill(background)
+    square_rect = pygame.Rect(chess_x - 2, chess_y - 2, square_width + 4, square_width + 4)
+    pygame.draw.rect(screen, board_border, square_rect, width = 2 )
+
     # Draw each square independently
     for row in range(rows):
         for column in range(columns):
@@ -77,46 +110,31 @@ def draw_chessboard(rows, columns):
             pygame.draw.rect(screen, color, square_rect)
 
             # Check chessboard configuration and blit the corresponding image
-            piece_key_tuple = chessboard[row][column]
-            piece_key = piece_key_tuple[0]+ str(piece_key_tuple[1])
-            piece = chessboard_dict[piece_key]
-            piece_image = None
+            value = row * 8 + column
+            piece_key = squares[value]
+            piece = chessboard[piece_key]
+            draw_piece(piece, square_rect)
 
-            if piece == "pawn_w":
-                piece_image = pawn_w
-            elif piece == "rook_w":
-                piece_image = rook_w
-            elif piece == "knight_w":
-                piece_image = knight_w
-            elif piece == "bishop_w":
-                piece_image = bishop_w
-            elif piece == "queen_w":
-                piece_image = queen_w
-            elif piece == "king_w":
-                piece_image = king_w
-            elif piece == "pawn_b":
-                piece_image = pawn_b
-            elif piece == "rook_b":
-                piece_image = rook_b
-            elif piece == "knight_b":
-                piece_image = knight_b
-            elif piece == "bishop_b":
-                piece_image = bishop_b
-            elif piece == "queen_b":
-                piece_image = queen_b
-            elif piece == "king_b":
-                piece_image = king_b
-            # Handle the case when the piece is not recognized
-            else:
-                continue
+    draw_rows()
+    draw_columns()
 
-            if piece != None:
-                # Blit the piece image
-                screen.blit(piece_image, square_rect.topleft)
+    if highlight:
+        highlight_square(mouse_x, mouse_y)
+
+# Function to draw pieces on the board
+def draw_piece(piece, square_rect):
+    global piece_image
+
+    # Get the piece image from the dictionary
+    piece_img = piece_image.get(piece)
+
+    if piece_img is not None:
+        # Blit the piece image
+        screen.blit(piece_img, square_rect.topleft)
 
 # Draw chessboard rows
 def draw_rows():
-    for i, row_value in enumerate(chessboard_rows):
+    for i, row_value in enumerate(RANK_NAMES):
         text = font.render(str(row_value), True, text_color)
         text_rect = text.get_rect(center=(chess_x - 30, chess_y + i * square_size + square_size // 2))
         screen.blit(text, text_rect)
@@ -125,7 +143,7 @@ def draw_rows():
 
 # Draw chessboard columns
 def draw_columns():
-    for i, col_value in enumerate(chessboard_columns):
+    for i, col_value in enumerate(FILE_NAMES):
         text = font.render(col_value, True, text_color)
         text_rect = text.get_rect(center=(chess_x + i * square_size + square_size // 2, chess_y - 30))
         screen.blit(text, text_rect)
@@ -143,6 +161,11 @@ def handle_events():
 
 #handle mouse clicks
 def handle_mouse_click():
+    global highlight, mouse_x, mouse_y
+
+    square_rect = pygame.Rect(chess_x - 2, chess_y - 2, square_width + 4, square_width + 4)
+    pygame.draw.rect(screen, white, square_rect, width = 2 )
+
     # Get the mouse coordinates
     mouse_x, mouse_y = pygame.mouse.get_pos()
 
@@ -152,18 +175,34 @@ def handle_mouse_click():
 
     # Check if the click is within the chessboard boundaries
     if 0 <= clicked_row < rows and 0 <= clicked_column < columns:
-        clicked_square_value = chessboard[clicked_row][clicked_column]
-        clicked_square_value = clicked_square_value[0]+ str(clicked_square_value[1])
-
+        value = clicked_row*8 + clicked_column
+        clicked_square_value = squares[value]
         print(f"Mouse clicked at Square : {clicked_square_value}")
+        #Change the border to white when clicked
+        highlight = True
+
     else:
         print(f"Mouse clicked at ({mouse_x}, {mouse_y})")
+        highlight = False
 
-while True:
+def highlight_square(mouse_x, mouse_y):
+    # Determine the clicked square
+    clicked_column = (mouse_x - chess_x) // square_size
+    clicked_row = (mouse_y - chess_y) // square_size
+
+    x = chess_x + (clicked_column * square_size)
+    y = chess_y + (clicked_row * square_size)
+
+    square_rect = pygame.Rect(x, y, square_size, square_size)
+    pygame.draw.rect(screen, gold, square_rect, width = 2 )
+
+def game():
     draw_chessboard(rows, columns)
-    draw_rows()
-    draw_columns()
     handle_events()
 
     # Update the display
     pygame.display.flip()
+
+while __name__ == "__main__":
+    game()
+
