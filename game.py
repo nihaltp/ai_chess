861528ee3b94966e80d2a58ba3972e3bd3ac706d
nihaltp <nihaltpnki@gmail.com>
2023-12-15 11:06:06 +0530

    modified

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import sys
+import chess
 import pygame
 from config import *
 
@@ -15,7 +16,7 @@ font = pygame.font.SysFont(None, 30)
 
 # Draw chessboard
 def draw_chessboard(ROWS, COLUMNS):
-    global highlight, mouse_x, mouse_y
+    global highlight #, mouse_x, mouse_y
 
     screen.fill(BACKGROUND)
     square_rect = pygame.Rect(chess_x - 2, chess_y - 2, SQUARE_WIDTH + 4, SQUARE_WIDTH + 4)
@@ -37,8 +38,8 @@ def draw_chessboard(ROWS, COLUMNS):
     draw_rows()
     draw_columns()
 
-    if highlight:
-        highlight_square(mouse_x, mouse_y)
+    # if highlight:
+    #     highlight_square(mouse_x, mouse_y)
 
 # Function to draw pieces on the board
 def draw_piece(piece, square_rect):
@@ -74,61 +75,153 @@ def draw_columns():
         screen.blit(text, text_rect)
 
 #handle events
-def handle_events():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            handle_mouse_click()
+# def handle_events():
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             pygame.quit()
+#             sys.exit()
+#         elif event.type == pygame.MOUSEBUTTONDOWN:
+#             handle_mouse_click()
 
 #handle mouse clicks
-def handle_mouse_click():
-    global highlight, mouse_x, mouse_y
+# def handle_mouse_click():
+#     global highlight, mouse_x, mouse_y
 
-    square_rect = pygame.Rect(chess_x - 2, chess_y - 2, SQUARE_WIDTH + 4, SQUARE_WIDTH + 4)
-    pygame.draw.rect(screen, WHITE, square_rect, width = 2 )
+#     square_rect = pygame.Rect(chess_x - 2, chess_y - 2, SQUARE_WIDTH + 4, SQUARE_WIDTH + 4)
+#     pygame.draw.rect(screen, WHITE, square_rect, width = 2 )
 
-    # Get the mouse coordinates
-    mouse_x, mouse_y = pygame.mouse.get_pos()
+#     # Get the mouse coordinates
+#     mouse_x, mouse_y = pygame.mouse.get_pos()
 
-    # Determine the clicked square
-    clicked_column = (mouse_x - chess_x) // SQUARE_SIZE
-    clicked_row = (mouse_y - chess_y) // SQUARE_SIZE
+#     # Determine the clicked square
+#     clicked_column = (mouse_x - chess_x) // SQUARE_SIZE
+#     clicked_row = (mouse_y - chess_y) // SQUARE_SIZE
 
-    # Check if the click is within the chessboard boundaries
-    if 0 <= clicked_row < ROWS and 0 <= clicked_column < COLUMNS:
-        value = clicked_row*8 + clicked_column
-        clicked_square_value = SQUARES[value]
-        clicked_piece = chessboard[clicked_square_value]
-        print(f"Mouse clicked at Square : {clicked_square_value}")
-        if clicked_piece != "NA":
-            print(f"Mouse clicked on : {clicked_piece}")
+#     # Check if the click is within the chessboard boundaries
+#     if 0 <= clicked_row < ROWS and 0 <= clicked_column < COLUMNS:
+#         value = clicked_row*8 + clicked_column
+#         clicked_square_value = SQUARES[value]
+#         clicked_piece = chessboard[clicked_square_value]
+#         print(f"Mouse clicked at Square : {clicked_square_value}")
+#         if clicked_piece != "NA":
+#             print(f"Mouse clicked on : {clicked_piece}")
 
-        #Change the border to WHITE when clicked
-        highlight = True
+#         #Change the border to WHITE when clicked
+#         highlight = True
 
-    else:
-        print(f"Mouse clicked at ({mouse_x}, {mouse_y})")
-        highlight = False
+#     else:
+#         print(f"Mouse clicked at ({mouse_x}, {mouse_y})")
+#         highlight = False
 
-def highlight_square(mouse_x, mouse_y):
-    # Determine the clicked square
-    clicked_column = (mouse_x - chess_x) // SQUARE_SIZE
-    clicked_row = (mouse_y - chess_y) // SQUARE_SIZE
+# def highlight_square(mouse_x, mouse_y):
+#     # Determine the clicked square
+#     clicked_column = (mouse_x - chess_x) // SQUARE_SIZE
+#     clicked_row = (mouse_y - chess_y) // SQUARE_SIZE
 
-    x = chess_x + (clicked_column * SQUARE_SIZE)
-    y = chess_y + (clicked_row * SQUARE_SIZE)
+#     x = chess_x + (clicked_column * SQUARE_SIZE)
+#     y = chess_y + (clicked_row * SQUARE_SIZE)
 
-    square_rect = pygame.Rect(x, y, SQUARE_SIZE, SQUARE_SIZE)
-    pygame.draw.rect(screen, GOLD, square_rect, width = 2 )
+#     square_rect = pygame.Rect(x, y, SQUARE_SIZE, SQUARE_SIZE)
+#     pygame.draw.rect(screen, GOLD, square_rect, width = 2 )
 
-def game():
+def update(board):
+    board_to_print = ((str(board)).replace(" ", "")).replace("\n", "")
+    print(board_to_print)
+    for i, j in zip(board_to_print, SQUARES):
+        print(i,j,end=" ")
+        # Get the piece image from the dictionary
+        chessboard[j] = PIECE_VALUE.get(i)
+        print(chessboard[j])
+
+def game(board):
+    update(board)
     draw_chessboard(ROWS, COLUMNS)
-    handle_events()
+    #handle_events()
 
     # Update the display
     pygame.display.flip()
 
-while __name__ == "__main__":
-    game()
+def get_player_names():
+    player1 = input("Enter Player 1's name: ")
+    player2 = input("Enter Player 2's name: ")
+    return player1, player2
+
+def play_game(player1, player2):
+    player1_moves = []  # Store Player 1's moves separately
+    player2_moves = []  # Store Player 2's moves separately
+    current_player = 0
+    players = [player1, player2]
+    board = chess.Board()
+
+    while not board.is_game_over():
+        game(board)
+        print(f"{players[current_player]}'s turn.")
+        move = (input("Enter your move (e.g., e2e4): ")).lower()
+
+
+        if move in ["exit", "stop"]:
+            break
+
+        else:
+            try:
+                move = chess.Move.from_uci(move)
+                print(f"\033[92mMove confirmed: {move.uci()}.\033[0m")  # Added move confirmation prompt
+                if move in board.legal_moves:
+                    if board.is_castling(move):  # Check for castling
+                        board.push(move)  # Perform castling
+                        if move.from_square == chess.E1 and move.to_square == chess.G1:  # Kingside castling for white
+                            board.push(chess.Move.from_uci("e1g1"))  # Move the King
+                            board.push(chess.Move.from_uci("h1f1"))  # Move the Rook
+
+                        elif move.from_square == chess.E1 and move.to_square == chess.C1:  # Queenside castling for white
+                            board.push(chess.Move.from_uci("e1c1"))  # Move the King
+                            board.push(chess.Move.from_uci("a1d1"))  # Move the Rook
+
+                        elif move.from_square == chess.E8 and move.to_square == chess.G8:  # Kingside castling for black
+                            board.push(chess.Move.from_uci("e8g8"))  # Move the King
+                            board.push(chess.Move.from_uci("h8f8"))  # Move the Rook
+
+                        elif move.from_square == chess.E8 and move.to_square == chess.C8:  # Queenside castling for black
+                            board.push(chess.Move.from_uci("e8c8"))  # Move the King
+                            board.push(chess.Move.from_uci("a8d8"))  # Move the Rook
+
+                    else:
+                        board.push(move)  # For normal moves
+
+                    if current_player == 0:
+                        player1_moves.append(move)  # Store Player 1's move
+                    else:
+                        player2_moves.append(move)  # Store Player 2's move
+
+                    current_player = 1 - current_player  # Switch players
+
+                    if board.is_checkmate():
+                        print(f"Checkmate! {players[current_player]} wins.")
+                        break
+                    elif board.is_stalemate():
+                        print("Stalemate! The game is a draw.")
+                        break
+                    elif board.is_insufficient_material():
+                        print("Insufficient material! The game is a draw.")
+                        break
+                    elif board.is_check():
+                        print(f"\033[91mCheck! {players[current_player]} is in check!\033[0m")
+                else:
+                    print("Invalid move! Try again.")
+            except ValueError:
+                print("Invalid move format! Use UCI format (e.g., e2e4).")
+
+
+    print("\033[93mGame over.\033[0m")
+    print("\033[95mResult: " + board.result() + "\033[0m")  # Enhanced game over message
+
+    play_again = input("Do you want to play again? Reply with 'Y' for yes: ")
+    if play_again.lower() in ["yes", "y"]:
+        change_names = input("Do you want to change your names? Reply with 'Y' for yes: ")
+        if change_names.lower() in ["yes", "y"]:
+            player1, player2 = get_player_names()
+        play_game(player1, player2)
+
+if __name__ == "__main__":
+    player1, player2 = get_player_names()
+    play_game(player1, player2)
