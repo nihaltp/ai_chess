660426c6aa872df92037b50f7e7258eda1da3626
nihaltp <nihaltpnki@gmail.com>
2023-12-17 22:39:55 +0530

    added BUTTONS

--- config.py
+++ config.py
@@ -27,12 +27,20 @@ BUTTON_UNDO_POS = (BUTTON_X, BUTTON_Y+150)
 BUTTON_DRAW_POS = (BUTTON_X, BUTTON_Y+200)
 BUTTON_STOP_POS = (BUTTON_X, BUTTON_Y+250)
 
+BUTTONS = [
+    "History",
+    "Hint",
+    "Undo",
+    "Draw",
+    "Stop"
+]
+
 BUTTON_POSITIONS = {
-    'History': BUTTON_HISTORY_POS,
-    'Hint': BUTTON_HINT_POS,
-    'Undo': BUTTON_UNDO_POS,
-    'Draw': BUTTON_DRAW_POS,
-    'Stop': BUTTON_STOP_POS
+    "History": BUTTON_HISTORY_POS,
+    "Hint": BUTTON_HINT_POS,
+    "Undo": BUTTON_UNDO_POS,
+    "Draw": BUTTON_DRAW_POS,
+    "Stop": BUTTON_STOP_POS
 }
 
 # Colours
@@ -104,14 +112,14 @@ RANK_NAMES = ["8", "7", "6", "5", "4", "3", "2", "1"]
 
 # Create a 2D list to represent the chessboard with values
 SQUARES = [
-    'a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8',
-    'a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7',
-    'a6', 'b6', 'c6', 'd6', 'e6', 'f6', 'g6', 'h6',
-    'a5', 'b5', 'c5', 'd5', 'e5', 'f5', 'g5', 'h5',
-    'a4', 'b4', 'c4', 'd4', 'e4', 'f4', 'g4', 'h4',
-    'a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'h3',
-    'a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2',
-    'a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1'
+    "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+    "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+    "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+    "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+    "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+    "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+    "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+    "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"
 ]
 
 SQUARES_NUMBER = [
